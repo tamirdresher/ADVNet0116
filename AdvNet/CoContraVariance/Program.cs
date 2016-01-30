186bdbaa04ca551212b30a3f00b8623304d10cb4
tamir.dresher@gmail.com <vhpupuyo>
2016-01-30 14:50:17 +0200

    examples from 28/01/2016

---
+++ AdvNet/CoContraVariance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoContraVariance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> sFunc = (s) => Console.WriteLine(s);
+            Action<object> oFunc = (o) => Console.WriteLine(o);
+
+            //since T is 'in' the action can be
+            //assigned to Action<G> where G:T
+            //in == upcast
+            sFunc = oFunc;
+
+            sFunc("123");
+           
+
+            //out
+            IEnumerable<object> objects =
+                new List<string>();
+
+
+            Func<object, string> f1 = x => x.ToString();
+            Func<string, object> f2 = f1;
+
+           
+        }
+    }
+}
---
+++ AdvNet/Grouping/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grouping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new[]
+            {
+                new Book() {Name = "11", Year = 2010},
+                new Book() {Name = "12", Year = 2011}
+            };
+
+            IEnumerable<IGrouping<int, Book>> groups = 
+                books.GroupBy(b => b.Year);
+
+            IEnumerable<IGrouping<int, Book>> yearGroups = 
+                from book in books
+                from book2 in books
+                let year= book.Year-book2.Year
+                group book by book.Year
+                into g 
+                select g;
+
+            var bookGroups = 
+                from g in yearGroups
+                orderby g.Key descending
+                select new {Year = g.Key, Books = g, Count=g.Count()};
+        }
+    }
+
+    class BookGroup
+    {
+        public int Year { get; set; }
+        public IEnumerable<Book> Books { get; set; } 
+    }
+
+    internal class Book
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
